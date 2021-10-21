255138500fd117a30eeaa8d6a91bd3e88a699789
Nikolai Saganenko <9952728@gmail.com>
2021-10-21 16:46:44 +0300

    Initial Commit

---
+++ Image scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image scroll
+//
+//  Created by Nick Sagan on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
