1eb3d30e16e2f56f2abbf936967224a7e306d6de
Nikolai Saganenko <9952728@gmail.com>
2021-10-22 10:48:16 +0300

    Add upload images

--- Image scroll/DetailViewController.swift
+++ Image scroll/DetailViewController.swift
@@ -16,19 +16,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //imageView.image =
-
+        if let image = selectedImage {
+            imageView.image = UIImage(named: image)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
