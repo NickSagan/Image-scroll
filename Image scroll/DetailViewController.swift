60c85e2a7e7bf03d062f6dab3268dc96aeb66231
Nikolai Saganenko <9952728@gmail.com>
2021-10-23 14:42:40 +0300

    Add UIActionViewController Add access request to plist

--- Image scroll/DetailViewController.swift
+++ Image scroll/DetailViewController.swift
@@ -18,12 +18,26 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let image = selectedImage {
             imageView.image = UIImage(named: image)
         }
         
         title = "Image \(imageNumber) of \(totalImageNumber)"
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
